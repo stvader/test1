6eac0d0901cc9cfae022d0f9fb41c0f802c25bd5
Vadim St <vadim_st_13@mail.ru>
2017-06-02 16:08:10 +0300

    slider slid.js

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	var slider = $('.js-slid');
+	var sliderImage = $('.slider__item');
+
+    slider.slick({
+    	autoplay: false,
+    	autoplaySpeed: 5000,
+    	arrows: false,
+    	dots: false,
+        initialSlide: 0
+    	
+
+    });    
+});
